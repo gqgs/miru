5c4ab0eb56f7a4623251386ec2b58e1d1543b2f1
Guilherme <gqgs@protonmail.com>
2020-04-25 15:45:28 -0300

    cmd: add profile flag

--- cmd/miru-insert/main.go
+++ cmd/miru-insert/main.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime/pprof"
 )
 
 type options struct {
 	db       string
 	folder   string
 	parallel uint
+	profile  bool
 }
 
 func main() {
@@ -17,7 +19,17 @@ func main() {
 	flag.StringVar(&o.db, "db", os.Getenv("MIRU_DB"), "database name")
 	flag.StringVar(&o.folder, "folder", ".", "target folder")
 	flag.UintVar(&o.parallel, "parallel", 10, "number of files to process in parallel")
+	flag.BoolVar(&o.profile, "cpuprofile", false, "create CPU profile")
 	flag.Parse()
+
+	if o.profile {
+		f, err := os.Create("cpuprofile")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	if err := insert(o); err != nil {
 		log.Fatal(err)
--- cmd/miru-search/main.go
+++ cmd/miru-search/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime/pprof"
 )
 
 type options struct {
@@ -12,16 +13,27 @@ type options struct {
 	accuracy uint
 	limit    uint
 	open     bool
+	profile  bool
 }
 
 func main() {
 	var o options
 	flag.StringVar(&o.db, "db", os.Getenv("MIRU_DB"), "database name")
-	flag.StringVar(&o.file, "file", "", "Target file|url")
-	flag.UintVar(&o.accuracy, "accuracy", 2, "Accuracy. Higher = more accurate = slower")
-	flag.UintVar(&o.limit, "limit", 10, "Number of closest matches to display")
-	flag.BoolVar(&o.open, "open", false, "Open closest match")
+	flag.StringVar(&o.file, "file", "", "target file|url")
+	flag.UintVar(&o.accuracy, "accuracy", 2, "accuracy. higher = more accurate = slower")
+	flag.UintVar(&o.limit, "limit", 10, "number of closest matches to display")
+	flag.BoolVar(&o.open, "open", false, "open closest match")
+	flag.BoolVar(&o.profile, "cpuprofile", false, "create CPU profile")
 	flag.Parse()
+
+	if o.profile {
+		f, err := os.Create("cpuprofile")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	if err := search(o); err != nil {
 		log.Fatal(err)
